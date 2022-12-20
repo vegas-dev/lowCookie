c8d40e5a14af1f57f816a3b7a521df5ab96029f5
Alexander Vetlugin <vetlugins@gmail.com>
2022-12-20 11:23:50 +0300

    default markup

--- src/VGLawCookie/js/VGLawCookie.js
+++ src/VGLawCookie/js/VGLawCookie.js
@@ -12,12 +12,17 @@ class VGLawCookie {
 	constructor (arg) {
 		this.container = document.getElementById('vg-lawCookie');
 
-		if (!this.container) {
-			console.error("Товарищ! Не найдена разметка под куки!");
-			return false;
-		}
-
-		this.settings = Object.assign({}, arg);
+		this.settings = Object.assign({
+			content: {
+				text: {
+					default: 'Наш сайт использует файлы «cookie» для удобства пользования веб-сайтом. «Cookie» представляют собой небольшие файлы, содержащие информацию о предыдущих посещениях веб-сайта. Продолжая использовать наш сайт, вы даете согласие на их обработку.',
+					btn: 'Я согласен(а)'
+				},
+				btn: {
+					classes: ['btn', 'btn-primary']
+				}
+			}
+		}, arg);
 
 		this.init();
 	}
@@ -26,12 +31,34 @@ class VGLawCookie {
 		let _this = this,
 			getCookie = Cookies.get('lawCookie');
 
+		if (!_this.container) {
+			_this.container = document.createElement('div');
+
+			_this.container.setAttribute('id', 'vg-lawCookie');
+			_this.container.classList.add('vg-lawCookie');
+
+			_this.container.insertAdjacentHTML('beforeend','<div class="vg-lawCookie--content">'+
+				'<p>' + _this.settings.content.text.default +'</p>' +
+				'<p><a href="#" data-lc-confirm>' + _this.settings.content.text.btn +'</a></p>' +
+			'</div>');
+
+			if (_this.settings.content.btn.classes.length) {
+				let btn = _this.container.querySelector('[data-lc-confirm]');
+
+				for (let cl of _this.settings.content.btn.classes) {
+					btn.classList.add(cl);
+				}
+			}
+
+			document.body.appendChild(_this.container);
+		}
+
 		if (!getCookie) {
 			setTimeout(() => {
 				_this.container.classList.add('show');
 			}, 500);
 
-			let btnConfirm = _this.container.querySelector('[data-ls-confirm]');
+			let btnConfirm = _this.container.querySelector('[data-lc-confirm]');
 			btnConfirm.onclick = function () {
 				_this.container.classList.remove('show');
 				Cookies.set('lawCookie', 'yes');
@@ -41,11 +68,5 @@ class VGLawCookie {
 		}
 	}
 }
-
-function onReady() {
-	new VGLawCookie();
-}
-
-document.addEventListener("DOMContentLoaded", onReady);
 
 export default VGLawCookie;
